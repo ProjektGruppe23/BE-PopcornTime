f77b0b2c71bef4e3d26041d409b9330371a31736
Thea6037 <thea6037@stud.kea.dk>
2023-10-03 09:48:30 +0200

    initial commit

---
+++ src/main/java/com/example/bepopcorntime/BePopcorntimeApplication.java
@@ -0,0 +1,15 @@
+package com.example.bepopcorntime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BePopcorntimeApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BePopcorntimeApplication.class, args);
+    }
+
+}
