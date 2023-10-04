f179668aff9c0049921209c0d8e330d784abf5b9
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2023-10-04 10:06:06 +0200

    dance

--- src/main/java/com/example/bepopcorntime/InitData.java
+++ src/main/java/com/example/bepopcorntime/InitData.java
@@ -208,5 +208,7 @@ public class InitData
         bookedSeat2.setSeat(seat2);
         bookedSeat2.setShowtime(showtime2);
         bookedSeatRepository.save(bookedSeat2);
+
+        //SPrint1
     }
 }
