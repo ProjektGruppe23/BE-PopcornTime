639940e19cccba43edec60eeac4b992d2c8826a8
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2023-10-05 10:23:50 +0200

    Added comments

--- src/test/java/com/example/bepopcorntime/movie/MovieRESTControllerTest.java
+++ src/test/java/com/example/bepopcorntime/movie/MovieRESTControllerTest.java
@@ -16,50 +16,58 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.*;
 
+// This annotation specifies that this is a Spring Boot test focused on just the web layer.
+// It also disables database auto-configuration to avoid connecting to the actual database.
 @WebMvcTest(controllers = MovieRESTController.class, excludeAutoConfiguration = {DataSourceAutoConfiguration.class})
 public class MovieRESTControllerTest {
 
+    // MockMvc is auto-wired to allow simulating HTTP requests against the controllers.
     @Autowired
     private MockMvc mockMvc;
 
+    // Mocking the actual repository using Mockito, integrated within the Spring context.
     @MockBean
     private MovieRepository movieRepository;
 
+    // Test case for the "getCurrentMovies" method in the MovieRESTController
     @Test
     void getCurrentMovies() throws Exception {
-        // Arrange
+        // Arrange section: Setting up test data and mock behavior
         Movie movie1 = new Movie();
         movie1.setStartDate(Date.valueOf("2023-01-01"));
 
         Movie movie2 = new Movie();
         movie2.setStartDate(Date.valueOf("2023-12-01"));
 
+        // Here, we simulate what the repository should return when findAll() is called.
         List<Movie> movies = Arrays.asList(movie1);
         Mockito.when(movieRepository.findAll()).thenReturn(movies);
 
-        // Act & Assert
-        mockMvc.perform(get("/movies/current"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].startDate", is("2023-01-01")));
+        // Act & Assert: Performing the mock HTTP request and verifying the response
+        mockMvc.perform(get("/movies/current"))  // Simulating a GET request to the endpoint
+                .andExpect(status().isOk())  // Expecting a 200 OK status
+                .andExpect(jsonPath("$", hasSize(1)))  // Expecting a JSON array of size 1
+                .andExpect(jsonPath("$[0].startDate", is("2023-01-01")));  // Expecting the startDate of the first object to be "2023-01-01"
     }
 
+    // Test case for the "getUpcomingMovies" method in the MovieRESTController
     @Test
     void getUpcomingMovies() throws Exception {
-        // Arrange
+        // Arrange section: Setting up test data and mock behavior
         Movie movie1 = new Movie();
         movie1.setStartDate(Date.valueOf("2023-01-01"));
 
         Movie movie2 = new Movie();
         movie2.setStartDate(Date.valueOf("2023-12-01"));
 
+        // Mocking what the repository should return when findAll() is called.
         List<Movie> movies = Arrays.asList(movie2);
         Mockito.when(movieRepository.findAll()).thenReturn(movies);
 
-        // Act & Assert
-        mockMvc.perform(get("/movies/upcoming"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].startDate", is("2023-12-01")));
+        // Act & Assert: Performing the mock HTTP request and verifying the response
+        mockMvc.perform(get("/movies/upcoming"))  // Simulating a GET request to the endpoint
+                .andExpect(status().isOk())  // Expecting a 200 OK status
+                .andExpect(jsonPath("$", hasSize(1)))  // Expecting a JSON array of size 1
+                .andExpect(jsonPath("$[0].startDate", is("2023-12-01")));  // Expecting the startDate of the first object to be "2023-12-01"
     }
 }
