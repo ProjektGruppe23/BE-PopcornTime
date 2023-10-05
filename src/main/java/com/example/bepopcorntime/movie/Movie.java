4cb08a1faa6a7203433dfa7287ebdd3a92ed20f4
Fred86r9 <118350827+Fred86r9@users.noreply.github.com>
2023-10-05 14:09:42 +0200

    Added session to selectedmovie getmapping

--- src/main/java/com/example/bepopcorntime/movie/Movie.java
+++ src/main/java/com/example/bepopcorntime/movie/Movie.java
@@ -1,6 +1,7 @@
 package com.example.bepopcorntime.movie;
 
 import com.example.bepopcorntime.age_limit.AgeLimit;
+import com.example.bepopcorntime.genre.Genre;
 import com.example.bepopcorntime.movie_genre.MovieGenre;
 import com.example.bepopcorntime.showtime.Showtime;
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -8,6 +9,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -27,10 +29,12 @@ public class Movie
     private String picture;
     private int length;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "movie")
-    @JsonBackReference
+    //@JsonBackReference
     private Set<Showtime> showtimes = new HashSet<>();
 
     @OneToMany(mappedBy = "movie")
+    //@JsonBackReference
     private Set<MovieGenre> movieGenres = new HashSet<>();
+
 
 }
--- src/main/java/com/example/bepopcorntime/movie/MovieRESTController.java
+++ src/main/java/com/example/bepopcorntime/movie/MovieRESTController.java
@@ -1,6 +1,9 @@
 package com.example.bepopcorntime.movie;
 
 
+import com.example.bepopcorntime.movie_genre.MovieGenre;
+import com.example.bepopcorntime.movie_genre.MovieGenreRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,13 +23,35 @@ public class MovieRESTController
     @Autowired
     MovieRepository movieRepository;
 
+    @Autowired
+    MovieGenreRepository movieGenreRepository;
+
     @GetMapping("/selectedmovie/{id}")
-    public ResponseEntity<Movie> getMovieById(@PathVariable int id)
+    public ResponseEntity<Movie> getSelectedMovie(@PathVariable int id, HttpSession session)
     {
         Optional<Movie> movieOpt = movieRepository.findById(id);
+
+
+
         if(movieOpt.isPresent())
         {
             Movie movie = movieOpt.get();
+
+            /*List<String> genreNames = new ArrayList<>();
+            for (MovieGenre movieGenre : movie.getMovieGenres()) {
+                genreNames.add(movieGenre.getGenre().getType());
+            }*/
+
+            session.setAttribute("movieId", movie.getId());
+            session.setAttribute("title", movie.getTitle());
+            session.setAttribute("endDate", movie.getEndDate());
+            session.setAttribute("startDate", movie.getStartDate());
+            session.setAttribute("picture", movie.getPicture());
+            session.setAttribute("ageLimit", movie.getAgeLimit());
+            session.setAttribute("length", movie.getLength());
+            session.setAttribute("genre", movie.getMovieGenres());
+
+
             return ResponseEntity.ok(movie);
         }
         else
