d087ffd898c78042d0bac336fa3f46ce564db33c
UlrikLn <ulrik9000@hotmail.com>
2023-10-05 11:37:32 +0200

    Made preparation for Sending movie details to Frontend

--- src/main/java/com/example/bepopcorntime/booked_seat/bookedSeatRESTController.java
+++ src/main/java/com/example/bepopcorntime/booked_seat/bookedSeatRESTController.java
@@ -1,5 +1,7 @@
 package com.example.bepopcorntime.booked_seat;
 
+import com.example.bepopcorntime.movie.Movie;
+import com.example.bepopcorntime.movie.MovieRESTController;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,12 +21,24 @@ public class bookedSeatRESTController
     @Autowired
     BookedSeatRepository bookedSeatRepository;
 
+    @Autowired
+    MovieRESTController movieRESTController;
 
 
     @GetMapping("/getBookedSeats")
     public ResponseEntity<List<BookedSeat>> getBookedSeats(Session session)
     {
-        List<BookedSeat> listOfBookedSeats = bookedSeatRepository.findBookedSeatByShowtime_Id(1);
+        List<BookedSeat> listOfBookedSeats = bookedSeatRepository.findBookedSeatByShowtime_Id(1); //session.get(showTimeId);
         return new ResponseEntity<>(listOfBookedSeats, HttpStatus.OK);
     }
+
+    /*
+    @GetMapping("/getMovieDetails")
+    public ResponseEntity<List<Movie>>  getMovieDetails(Session session)
+    {
+        List<Movie> listOfMovieDetails = session.get(all);
+        return new ResponseEntity<>(listOfMovieDetails, HttpStatus.OK);
+    }
+
+    */
 }
