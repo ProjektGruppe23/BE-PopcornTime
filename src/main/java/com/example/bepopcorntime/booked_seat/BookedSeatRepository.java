c6e26b72faf6d95c3ac2d7d748a56e1a0628d3c8
Thea6037 <thea6037@stud.kea.dk>
2023-10-12 13:26:21 +0200

    confirmed booking works

--- src/main/java/com/example/bepopcorntime/booked_seat/BookedSeatRepository.java
+++ src/main/java/com/example/bepopcorntime/booked_seat/BookedSeatRepository.java
@@ -1,12 +1,19 @@
 package com.example.bepopcorntime.booked_seat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface BookedSeatRepository extends JpaRepository<BookedSeat, Integer>
 {
     List<BookedSeat> findBookedSeatByShowtime_Id(int showtimeData);
+   // int findBookingIdByShowtimeId(int showtimeId);
+
+    @Query("SELECT b.booking.id FROM BookedSeat b WHERE b.showtime.id = :showtimeId")
+    List<Integer> findBookingIdByShowtimeId(@Param("showtimeId") int showtimeId);
 
     String findBySeat(String seat);
 
